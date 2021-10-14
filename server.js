63554e00e266494d9ed15e025bb6dc5fff9315ab
tlaze <tomlazore@gmail.com>
2021-10-14 13:35:20 -0400

    index.html and notes.html linked with server.js to display on localhost

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const PORT = 3001;
+const path = require('path');
 
 app.use(express.static('public'));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'develop/public/index.html')));
+
+app.get('/notes', (req,res) => res.sendFile(path.join(__dirname, 'develop/public/notes.html')));
 
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT}`);
