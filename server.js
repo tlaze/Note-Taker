f33b876ec8edf8c781d5c373d566d40c231c757a
tlaze <tomlazore@gmail.com>
2021-10-14 14:38:11 -0400

    fixed express.static file syntax

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const fs = require('fs');
 const PORT = 3001;
 const path = require('path');
 
-app.use(express.static('public'));
+app.use(express.static('Develop/public'));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'develop/public/index.html')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+    });
 
-app.get('/notes', (req,res) => res.sendFile(path.join(__dirname, 'develop/public/notes.html')));
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, 'develop/public/notes.html'))
+    });
 
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT}`);
