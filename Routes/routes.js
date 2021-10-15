c29f8001cf2b66745907231ac0ddc6806519c428
tlaze <tomlazore@gmail.com>
2021-10-14 22:57:03 -0400

    added comments

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,8 +1,12 @@
+//declares the path
 const path = require('path');
+//declares access to other files
 const route = require('express').Router();
+//allows use of file system
 const fs = require('fs');
 
 //API Routes
+//reads db.jsonfile and displays the notes data
 route.get('/api/notes', (req,res) => {
     fs.readFile(path.join(__dirname, "../db/db.json"), (err,data) => {
         if(err){
@@ -14,7 +18,9 @@ route.get('/api/notes', (req,res) => {
     });
 });
 
+//new note created after saving note
 route.post('/api/notes', (req,res) => {
+    //reads through db.json and pushes object data into an array
     fs.readFile(path.join(__dirname, "../db/db.json"), (err, data) =>{
         if(err){
             console.error(err);
@@ -25,7 +31,7 @@ route.post('/api/notes', (req,res) => {
             let notesArray = [];
 
             notes.push(req.body);
-            
+            //parses through object data. obtains title text and an id and pushes it into notesArray
             for(var i = 0; i < notes.length; i++){
                 let newNote = {
                     title: notes[i].title,
@@ -34,6 +40,7 @@ route.post('/api/notes', (req,res) => {
                 };
                 notesArray.push(newNote);
             }
+            //writes new db.json file of updated notes object. Includes new note now
             fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notesArray, null, 2), (err) => {
                 if(err){
                     console.error(err);
@@ -46,7 +53,9 @@ route.post('/api/notes', (req,res) => {
     });
 });
 
+//deletes notes when clicking delete button
 route.delete("/api/notes/:id", (req,res) => {
+    //obtains the id of the note that user wants deleted
     const id = parseInt(req.params.id);
     console.log(id);
 
@@ -72,7 +81,7 @@ route.delete("/api/notes/:id", (req,res) => {
                 }
             }
             console.log(notesArray);
-
+            //writes new db.json file that no longer includes the deleted note
             fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notesArray, null, 2), (err) =>{
                 if(err){
                     console.error(err);
@@ -85,14 +94,15 @@ route.delete("/api/notes/:id", (req,res) => {
     })
 })
 
-
 //HTML Routes
+//displays notes.html on the /notes route
 route.get('/notes', (req,res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
-    
+  
+//any other routes will end up displaying index.html
 route.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
-
+//exports Router() to use on other files
 module.exports = route;
--- server.js
+++ server.js
@@ -1,15 +1,21 @@
+//declares express dependency
 const express = require('express');
+//creates link to routes.js
 const routes = require("./Routes/routes.js");
-
+//sets app to use express
 const app = express();
+//uses heroku pork or port 3001
 const PORT = process.env.PORT || 3001;
 
+//lets you parse object data within files
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+//sets express.static to the public folder
 app.use(express.static('public'));
-
+//routing option when loading page, routes found on routes.js
 app.use("/", routes);
 
+//tells you what port the application is running on
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT}`);
 });
