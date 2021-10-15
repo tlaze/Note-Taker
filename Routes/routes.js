0dccc25264bbdec01883e45ca390f68347a32850
tlaze <tomlazore@gmail.com>
2021-10-14 21:42:24 -0400

    post route adds a new note object to db.json

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,11 +1,55 @@
 const path = require('path');
 const route = require('express').Router();
+const fs = require('fs');
+const noteData = require('../db/db.json');
 
-// route.get('/api/notes', (req,res) => {
-//     res.json(noteData);
-// });
+//API Routes
+route.get('/api/notes', (req,res) => {
+    fs.readFile(path.join(__dirname, "../db/db.json"), (err,data) => {
+        if(err){
+            console.error(err);
+        }
+        else{
+            res.json(JSON.parse(data));
+        }
+    });
+});
+
+route.post('/api/notes', (req,res) => {
+    fs.readFile(path.join(__dirname, "../db/db.json"), (err, data) =>{
+        if(err){
+            console.error(err);
+        }
+        else{
+            const notes = JSON.parse(data);
+            console.log(notes);
+
+            const notesArray = [];
+
+            notes.push(req.body);
+            
+            for(var i = 0; i < notes.length; i++){
+                const newNote = {
+                    title: notes[i].title,
+                    text: notes[i].text,
+                    id: i
+                };
+                notesArray.push(newNote);
+            }
+            fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notesArray, null, 2), (err) => {
+                if(err){
+                    console.error(err);
+                }
+                else{
+                    res.json(req.body);
+                }
+            });
+        }
+    });
+});
 
 
+//HTML Routes
 route.get('/notes', (req,res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
